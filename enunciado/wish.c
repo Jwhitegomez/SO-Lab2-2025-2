2cdb7b6e92311d4976c99f3db2c665c9619ad884
Davidson Pérez <trosquinalidad132@gmail.com>
2025-10-26 21:59:53 -0500

    .

--- enunciado/wish.c
+++ enunciado/wish.c
@@ -1,179 +1,185 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/wait.h>
 #include <fcntl.h>
-#include <stdbool.h>
 
-#define MAX_PATH 100
-#define MAX_ARGS 100
+#define MAX_ARGS 64
+#define MAX_PATHS 64
 
 char error_message[30] = "An error has occurred\n";
-char *shell_path[MAX_PATH] = {"/bin", NULL};
-
-// ----------------------------------------------------
+char *shell_path[MAX_PATHS];
+int batch_mode = 0;
 
 void print_error() {
     write(STDERR_FILENO, error_message, strlen(error_message));
 }
 
-// ----------------------------------------------------
-
-bool is_builtin(char *cmd) {
-    return (strcmp(cmd, "exit") == 0 ||
-            strcmp(cmd, "cd") == 0 ||
-            strcmp(cmd, "path") == 0);
+/** Inserta espacios alrededor de '&' */
+void normalize_ampersand(char *line) {
+    char tmp[1024];
+    int j = 0;
+    for (int i = 0; line[i]; i++) {
+        if (line[i] == '&') {
+            tmp[j++] = ' ';
+            tmp[j++] = '&';
+            tmp[j++] = ' ';
+        } else {
+            tmp[j++] = line[i];
+        }
+    }
+    tmp[j] = '\0';
+    strcpy(line, tmp);
 }
 
-// ----------------------------------------------------
-
+/** Comandos internos */
 int builtin_cd(char **args) {
-    if (args[1] == NULL || args[2] != NULL) {
-        print_error();
-        return 1;
-    }
-    if (chdir(args[1]) != 0) {
-        print_error();
-    }
-    return 1;
-}
-
-int builtin_exit(char **args) {
-    if (args[1] != NULL) {
-        print_error();
-        return 1;
-    }
-    exit(0);
+    if (args[1] == NULL || args[2] != NULL) return -1;
+    if (chdir(args[1]) != 0) return -1;
+    return 0;
 }
 
 int builtin_path(char **args) {
-    for (int i = 0; i < MAX_PATH; i++)
+    for (int i = 0; i < MAX_PATHS; i++) {
+        free(shell_path[i]);
         shell_path[i] = NULL;
-
-    int i = 1;
-    while (args[i] != NULL && i < MAX_PATH) {
-        shell_path[i - 1] = args[i];
-        i++;
     }
-    return 1;
+    int idx = 1;
+    int j = 0;
+    while (args[idx] != NULL && j < MAX_PATHS - 1) {
+        shell_path[j++] = strdup(args[idx++]);
+    }
+    shell_path[j] = NULL;
+    return 0;
 }
 
-// ----------------------------------------------------
-
-char *find_executable(char *cmd) {
-    static char fullpath[256];
-
-    for (int i = 0; shell_path[i] != NULL; i++) {
-        snprintf(fullpath, sizeof(fullpath), "%s/%s",
-                 shell_path[i], cmd);
-        if (access(fullpath, X_OK) == 0)
-            return fullpath;
+/** Manejar redirección */
+int setup_redirection(char **args) {
+    int redirect_count = 0, idx = -1;
+    for (int i = 0; args[i] != NULL; i++) {
+        if (strcmp(args[i], ">") == 0) {
+            redirect_count++;
+            idx = i;
+        }
     }
-    return NULL;
+    if (redirect_count == 0) return 0;
+
+    if (redirect_count > 1 || idx == 0 || args[idx+1] == NULL || args[idx+2] != NULL) {
+        return -1;
+    }
+
+    int fd = open(args[idx+1], O_CREAT | O_WRONLY | O_TRUNC, 0666);
+    if (fd < 0) return -1;
+
+    if (dup2(fd, STDOUT_FILENO) < 0 || dup2(fd, STDERR_FILENO) < 0) {
+        close(fd);
+        return -1;
+    }
+    close(fd);
+    args[idx] = NULL;
+    return 0;
 }
 
-// ----------------------------------------------------
+/** Ejecutar comando externo */
+pid_t execute_external(char **args) {
+    if (shell_path[0] == NULL) {
+        print_error();
+        return -1;
+    }
 
-int run_external(char **args, char *outfile) {
+    int red = setup_redirection(args);
+    if (red == -1) {
+        print_error();
+        return -1;
+    }
+
     pid_t pid = fork();
     if (pid == 0) {
-        if (outfile != NULL) {
-            int fd = open(outfile, O_CREAT | O_WRONLY | O_TRUNC, 0666);
-            if (fd < 0) {
-                print_error();
-                exit(1);
+        for (int i = 0; shell_path[i] != NULL; i++) {
+            char full[512];
+            snprintf(full, sizeof(full), "%s/%s", shell_path[i], args[0]);
+            if (access(full, X_OK) == 0) {
+                execv(full, args);
+                break;
             }
-            dup2(fd, STDOUT_FILENO);
-            dup2(fd, STDERR_FILENO);
-            close(fd);
         }
-
-        char *exec_path = find_executable(args[0]);
-        if (!exec_path) {
-            print_error();
-            exit(1);
-        }
-
-        execv(exec_path, args);
         print_error();
         exit(1);
-
     } else if (pid < 0) {
         print_error();
+        return -1;
     }
     return pid;
 }
 
-// ----------------------------------------------------
-
+/** Analizar línea con múltiples & */
 void execute_line(char *line) {
-    char *commands[50];
+    char *commands[MAX_ARGS];
     int num_cmds = 0;
+    pid_t pids[MAX_ARGS];
 
-    char *token = strtok(line, "&");
-    while (token != NULL) {
-        commands[num_cmds++] = token;
-        token = strtok(NULL, "&");
+    normalize_ampersand(line);
+
+    char *cmd = strtok(line, "&");
+    while (cmd != NULL && num_cmds < MAX_ARGS - 1) {
+        commands[num_cmds++] = cmd;
+        cmd = strtok(NULL, "&");
     }
-
-    pid_t pids[num_cmds];
 
     for (int i = 0; i < num_cmds; i++) {
-        char *cmd = commands[i];
-
         char *args[MAX_ARGS];
-        char *outfile = NULL;
+        int n = 0;
 
-        int j = 0;
-        char *tok = strtok(cmd, " \t\n");
-        while (tok != NULL) {
-            if (strcmp(tok, ">") == 0) {
-                tok = strtok(NULL, " \t\n");
-                if (tok == NULL || strtok(NULL, " \t\n") != NULL) {
-                    print_error();
-                    return;
-                }
-                outfile = tok;
-                break;
-            }
-            args[j++] = tok;
+        char *tok = strtok(commands[i], " \t\n");
+        while (tok != NULL && n < MAX_ARGS - 1) {
+            args[n++] = tok;
             tok = strtok(NULL, " \t\n");
         }
-        args[j] = NULL;
+        args[n] = NULL;
 
-        if (args[0] == NULL)
-            return;
-
-        if (is_builtin(args[0])) {
-            if (strcmp(args[0], "cd") == 0) builtin_cd(args);
-            else if (strcmp(args[0], "exit") == 0) builtin_exit(args);
-            else if (strcmp(args[0], "path") == 0) builtin_path(args);
+        if (args[0] == NULL) {
             pids[i] = -1;
-        } else {
-            if (shell_path[0] == NULL) {
+            continue;
+        }
+
+        if (strcmp(args[0], "exit") == 0) {
+            if (args[1] != NULL) {
                 print_error();
-                pids[i] = -1; // no hay proceso para esperar
-                continue;    // NO ejecutar ni forzar execv
+            } else {
+                exit(0);
             }
-            pids[i] = run_external(args, outfile);
+            pids[i] = -1;
+        }
+        else if (strcmp(args[0], "cd") == 0) {
+            if (builtin_cd(args) == -1) print_error();
+            pids[i] = -1;
+        }
+        else if (strcmp(args[0], "path") == 0) {
+            if (builtin_path(args) == -1) print_error();
+            pids[i] = -1;
+        }
+        else {
+            pids[i] = execute_external(args);
         }
     }
 
-    for (int i = 0; i < num_cmds; i++)
-        if (pids[i] > 0) waitpid(pids[i], NULL, 0);
+    for (int i = 0; i < num_cmds; i++) {
+        if (pids[i] > 0)
+            waitpid(pids[i], NULL, 0);
+    }
 }
 
-// ----------------------------------------------------
-
+/** MAIN */
 int main(int argc, char *argv[]) {
+    FILE *input = stdin;
+    shell_path[0] = strdup("/bin");
+    shell_path[1] = NULL;
+
     if (argc > 2) {
         print_error();
         exit(1);
     }
-
-    FILE *input = stdin;
-    bool batch = false;
 
     if (argc == 2) {
         input = fopen(argv[1], "r");
@@ -181,16 +187,21 @@ int main(int argc, char *argv[]) {
             print_error();
             exit(1);
         }
-        batch = true;
+        batch_mode = 1;
     }
 
     char *line = NULL;
     size_t len = 0;
 
     while (1) {
-        if (!batch) printf("wish> ");
-        if (getline(&line, &len, input) == -1)
+        if (!batch_mode) {
+            printf("wish> ");
+            fflush(stdout);
+        }
+
+        if (getline(&line, &len, input) == -1) {
             exit(0);
+        }
 
         execute_line(line);
     }
